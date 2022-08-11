59876e3ad725069c90cec0a74172d71d36dc9f5a
Clemerson Lucas <87621463+lucasclemerson@users.noreply.github.com>
2022-08-11 16:07:14 -0300

    Update main.js
    codigo para verificar a data de meses

--- main.js
+++ main.js
@@ -7,19 +7,39 @@ function calcular (){
 
 	/**magic number**/
 	var magic_number = parseInt(valor_cota/valor_dividendo);
-	document.getElementById('magic_number').innerText=magic_number;
+	document.getElementById('magic_number').innerText=magic_number + " cota(s)";
 
 
 	/**meses até a renda passiva**/
 	var valor_passivo = parseInt(valor_renda_passiva/valor_dividendo);
-	document.getElementById('meses_renda_passiva').innerText=valor_passivo;
+	document.getElementById('meses_renda_passiva').innerText=valor_passivo + " R$";
 
 	/**renda final**/
 	var renda_final = parseInt(valor_cota*valor_passivo);
-	document.getElementById('valor_final').innerText=renda_final;
+	document.getElementById('valor_final').innerText=renda_final + " R$";
 
 
 	/**tempo para adquirir**/
-	var tempo_adquirir = parseInt(valor_passivo/12);
-	document.getElementById('tempo_adquirir').innerText=tempo_adquirir;
+	meses = 0;
+	cont = magic_number;
+	cotas_compradas = parseInt(n_cotas);
+	minhas_cotas = 0;
+	acm = 0;
+	meses_totais = 0;
+
+	do{
+		do{
+			meses = meses + 1;
+			minhas_cotas = minhas_cotas + cotas_compradas;
+		}while(minhas_cotas < cont);
+
+		acm = acm + minhas_cotas;
+		meses_totais = meses_totais + meses;
+		cotas_compradas = cotas_compradas + 1;
+		meses = 0;
+		minhas_cotas = 0;
+	}while(acm <= 3000);
+	meses_totais = meses_totais / 12;
+
+	document.getElementById('tempo_adquirir').innerText=meses_totais;
 }
